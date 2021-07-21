4b67fcda751f1a208022c20ecc8badebfdb8acc6
damatgre <gregdamato2@gmail.com>
2021-07-21 17:48:40 -0400

    initial User, Post, Comment models

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controllers');
+//const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-app.use(routes);
+//app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
