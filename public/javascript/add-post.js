0b87475f42362f9a7a9094d933cc64e566b24879
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-25 15:33:12 -0400

    downloaded link generator - adding code for posting links

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -3,6 +3,7 @@ async function newFormHandler(event) {
   
     const title = document.querySelector('input[name="post-title"]').value;
     const content = document.querySelector('input[name="content"]').value;
+    
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
