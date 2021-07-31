ae003c1efb033ecc11a17664a68ae8b32752b364
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-30 20:48:20 -0400

    editing

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -20,7 +20,7 @@ async function newFormHandler(event) {
     if (response.ok) {
       console.log("===============================================")
       console.log(response);
-      document.location.replace('/dashboard/');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
