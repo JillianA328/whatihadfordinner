fbb0d992b4a96065e4c940a60f7b3ec5dd4a5115
damatgre <gregdamato2@gmail.com>
2021-07-31 10:57:06 -0400

    fixed comments

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,12 +1,14 @@
 async function commentFormHandler(event) {
     event.preventDefault();
   
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const comment_text = document.querySelector('#comment').value.trim();
   
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-  
+
+  console.log(comment_text);
+
     if (comment_text) {
         const response = await fetch('/api/comments', {
           method: 'POST',
@@ -16,10 +18,14 @@ async function commentFormHandler(event) {
           }),
           headers: {
             'Content-Type': 'application/json'
-          }
+          } 
+          
         });
+
+      console.log(response)
       
         if (response.ok) {
+          
           document.location.reload();
         } else {
           alert(response.statusText);
